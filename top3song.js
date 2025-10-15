e01901a4dc72c4dbdfc61f2509cd7b0d0d4b7be3
slspstudios-sudo <slspstudios@gmail.com>
2025-10-16 00:15:05 +0200

    Update top3song.js

--- top3song.js
+++ top3song.js
@@ -1,10 +1,10 @@
-const top3 = playlist.slice(0, 3); // uzima prve tri pjesme iz playlist
-
-const top3Container = document.getElementById("top3-list");
-
-top3.forEach((song, i) => {
-  const li = document.createElement("li");
-  li.textContent = `${song.title}`;
-  li.onclick = () => playSong(i); // koristi istu funkciju playSong iz g-player.js
-  top3Container.appendChild(li);
+window.addEventListener('DOMContentLoaded', () => {
+  const top3 = playlist.slice(0, 3);
+  const top3Container = document.getElementById("top3-list");
+  top3.forEach((song, i) => {
+    const li = document.createElement("li");
+    li.textContent = song.title;
+    li.onclick = () => playSong(i);
+    top3Container.appendChild(li);
+  });
 });
